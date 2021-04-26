6945184f54d29533cd6934a99c843462c2769e51
BurnerReborn <burnerreborn24@gmail.com>
2021-04-26 15:15:12 -0400

    builds and ui positioning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using FMOD.Studio;
-using UnityEditor.IMGUI.Controls;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
